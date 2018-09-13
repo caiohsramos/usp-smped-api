842f8474a0f9306456137ef88de55c02fb862244
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-09-13 10:48:40 -0300

    Forms can now be created.

--- app/api/resources/forms.py
+++ app/api/resources/forms.py
@@ -1,26 +1,60 @@
 forms = {
-            'resource_methods': ['GET', 'POST'],
-            'item_methods': [ 'GET', 'PUT', 'PATCH', 'DELETE' ],
-            'allowed_roles': ['superuser', 'admin'],
+    'resource_methods': ['GET', 'POST'],
+    'public_item_methods': ['GET'],
+    'item_methods': [ 'GET', 'PUT', 'PATCH', 'DELETE' ],
+    'allowed_roles': ['superuser', 'admin'],
+    'schema': {
+        'version': {
+            'type': 'string',
+            'required': True
+        },
+        'owner': {
+            'type': 'objectid',
+            'data_relation': {
+                'resource': 'accounts',
+                'field': '_id',
+            },
+            'required': True
+        },
+        'name': {
+            'type': 'string',
+            'minlength': 5,
+            'required': True
+        },
+        'office': {
+            'type': 'string',
+            'allowed': ['smped', 'smdhc'],
+            'required': True
+        },
+        'activity': {
+            'type': 'string',
+            'required': True
+        },
+        'fields': {
+            'type': 'list',
             'schema': {
-                'version': {
-                    'type': 'string'
-                },
-                'owner': {
-                    'type': 'objectid',
-                    'data_relation': {
-                        'resource': 'accounts',
-                        'field': '_id',
+                'type': 'dict',
+                'schema': {
+                    'label': {
+                        'type': 'string',
+                        'required': True,
+                        'unique': True
                     },
-                    'required': True
-                },
-                'name': {
-                    'type': 'string',
-                    'minlength': 5
-                },
-                'office': {
-                    'type': 'string',
-                    'allowed': ['']
+                    'type': {
+                        'type': 'string',
+                        'allowed': ['number', 'text'],
+                        'required': True
+                    },
+                    'required': {
+                        'type': 'boolean',
+                        'required': True
+                    },
+                    'order': {
+                        'type': 'integer',
+                        'required': True
+                    }
                 }
             }
         }
+    }
+}
