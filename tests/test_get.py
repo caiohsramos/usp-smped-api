0d4f356754e8e95f91ce50d21364daef4973ae62
gabriely.pereira <gabriely.pereira@usp.br>
2018-10-03 16:04:22 -0300

    Criação do teste 'test_public_get' #13
    Signed-off-by: Caio Ramos <caioramos97@gmail.com>
    Signed-off-by: Gabriely Pereira <gabriely.pereira@usp.br>

--- tests/test_get.py
+++ tests/test_get.py
@@ -9,3 +9,16 @@ def test_get_token(client):
     rv = client.post('/auth/tokens', json={'username':'user','password':'password'})
     resp = json.loads(rv.data)
     assert resp['access_token'] and resp['refresh_token']
+
+def test_get_refresh(client):
+    rv = client.post('/auth/tokens', json={'username':'user','password':'password'})
+    resp = json.loads(rv.data)
+    refresh = resp['refresh_token']
+    rv = client.post('/auth/refresh', json={'refresh_token': refresh})
+    resp = json.loads(rv.data)
+    assert resp['access_token']
+
+def test_public_get(client):
+    rv = client.get('/forms')
+    resp = json.loads(rv.data)
+    assert resp["_items"] 
